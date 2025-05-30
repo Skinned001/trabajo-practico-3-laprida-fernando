f4bba8dc45414f16174d94247cb2e0a1ad816aab
Fernando <psamathe001@gmail.com>
2025-05-30 11:07:58 -0300

    estructura base satisfactoria, mas estilo bootstrap necesarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,9 @@
-// fetch("https://dragonball-api.com/api/characters")
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Error en la API");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => console.log(data.items))
-//   .catch((error) => console.log(error));
+
 
 const btnBuscar = document.getElementById("btn-buscar");
 const contenedorPadre = document.getElementById("contenedor-data");
-const urlDragonBall = "https://dragonball-api.com/api/characters";
-const urlHarryPotter = "https://hp-api.onrender.com/api/characters";
+const urlDragonBall = "https://dragonball-api.com/api/characters?limit=58";
+
 
 const cargarDatos = async (url) => {
   try {
@@ -48,30 +40,6 @@ const verDetalles = async (id) => {
   }
 };
 
-// para harry potter
-// btnBuscar.addEventListener("click", async () => {
-//   const dataPersonajes = await cargarDatos(urlHarryPotter);
-
-//   dataPersonajes.forEach((personaje) => {
-//     contenedorPadre.innerHTML += `
-//         <div class="col-3 pb-2 d-flex justify-content-center">
-//           <div class="card">
-//             <img
-//               class="card-img-top"
-//               src=${personaje.image}
-//             />
-//             <div class="card-body">
-//               <h5 class="card-title">${personaje.name}</h5>
-//               <p class="card-text">${personaje.race || "Sin Raza"} - ${
-//       personaje.gender
-//     }</p>
-//             </div>
-//           </div>
-//         </div>
-//     `;
-//   });
-// });
-
 // para dragon ball
 btnBuscar.addEventListener("click", async () => {
   const data = await cargarDatos(urlDragonBall);
@@ -80,12 +48,13 @@ btnBuscar.addEventListener("click", async () => {
   console.log(dataPersonajes);
 
   dataPersonajes.forEach((personaje) => {
+ 
     contenedorPadre.innerHTML += `
           <div class="col-3 pb-2 d-flex justify-content-center" data-id=${personaje.id}>
             <div class="card">
               <img
                 class="card-img-top"
-                src=${personaje.image}
+                src="${personaje.image}"
               />
               <div class="card-body">
                 <h5 class="card-title">${personaje.name}</h5>
